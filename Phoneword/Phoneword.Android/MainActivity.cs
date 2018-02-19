af71a370641a8429f34671784855879f2e391b9a
Beniamino Negrini <beniamino.negrini@nttdata.com>
2018-02-19 16:55:59 +0100

    fine

--- Phoneword/Phoneword.Android/MainActivity.cs
+++ Phoneword/Phoneword.Android/MainActivity.cs
@@ -1,10 +1,5 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace Phoneword.Droid
@@ -12,6 +7,8 @@ namespace Phoneword.Droid
     [Activity(Label = "Phoneword", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        internal static MainActivity Instance { get; private set; }
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -19,9 +16,9 @@ namespace Phoneword.Droid
 
             base.OnCreate(bundle);
 
+            Instance = this;
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
     }
 }
-
